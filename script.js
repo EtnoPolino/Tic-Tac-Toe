0debbd4b479f59cb917eac3a6d19ffa21534a46f
Etno Polino <99446834+EtnoPolino@users.noreply.github.com>
2022-08-30 18:21:45 +0200

    Add files via upload
    html css

---
+++ script.js
@@ -0,0 +1,10 @@
+/* debut 30/08/2022 à 11h03 - 11h47    https://www.google.com/search?client=firefox-b-e&q=switch+players+js 
+https://github.com/garyarzuma
+https://garyarzuma.github.io/Tic-Tac-Toe/ */
+
+const log = console.log;
+const gameboard = ["0", "1", "2",
+                   "3", "4", "5",
+                   "6", "7", "8",];
+
+log(gameboard[4])
