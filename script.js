4f1b4b9da7cd85174e1014111aa29b7332f63427
Etno Polino <99446834+EtnoPolino@users.noreply.github.com>
2022-09-02 18:19:44 +0200

    Add files via upload
    some css to do and reset button js to do

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ https://garyarzuma.github.io/Tic-Tac-Toe/ */
 const log = console.log;
 const logT = console.table;
 
-
-
 /*-----------------------------------------------------------------------*/
 
 const Player = function(name, signe){
@@ -20,8 +18,99 @@ const Player = function(name, signe){
         return signe;
     }
 
-   return {getName, setName, getSigne}
+   return {getName, setName, getSigne};
 }
+
+
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
+
+
+const Gameboard = (function(){
+
+    const winPossibilities = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    const squareGrid = document.querySelectorAll('.square');
+    const playerTurn = document.querySelector('article > span');
+    let players = [player1.getSigne(), player2.getSigne()];
+    let activePlayer = 0;
+
+    const Xres = [];
+    const Ores = [];
+
+
+    const checkWin = function(arg, squareId){
+        let isMarkWinning;
+    
+        if(arg == 'X'){
+            Xres.push(squareId);
+            isMarkWinning = Module.compareWinPossibilityToXOresult(winPossibilities, Xres);
+            if(isMarkWinning == true){
+                return true;
+            }            
+        }else{
+            Ores.push(squareId);
+            isMarkWinning = Module.compareWinPossibilityToXOresult(winPossibilities, Ores);
+            if(isMarkWinning == true){
+                return true;
+            }
+        }
+    }
+
+    const changeTurn = function(e){
+        let squareClickedId = e.target.dataset.square;
+        const squareClicked = document.getElementById(squareClickedId)
+        
+        if(activePlayer == 0){
+            if(squareClicked.innerText.includes('X') || squareClicked.innerText.includes('O')){
+                return;
+            }else{
+                squareClicked.innerText = players[activePlayer];
+                checkWin(players[activePlayer], squareClickedId);
+                if(checkWin(players[activePlayer], squareClickedId)){
+                    for(let i = 0; i < squareGrid.length; i++){
+                        squareGrid[i].removeEventListener('click', changeTurn);
+                        playerTurn.innerText = `${player1.getName()} WON`;
+                    }
+                }else{
+                    playerTurn.innerText = `${player2.getName()} turn`;
+                    activePlayer = 1; 
+                }               
+            }
+        }else{
+            if(squareClicked.innerText.includes('X') || squareClicked.innerText.includes('O')){
+                return;
+            }else{
+                squareClicked.innerText = players[activePlayer];
+                checkWin(players[activePlayer], squareClickedId);
+                if(checkWin(players[activePlayer], squareClickedId)){
+                    for(let i = 0; i < squareGrid.length; i++){
+                        squareGrid[i].removeEventListener('click', changeTurn);
+                        playerTurn.innerText = `${player2.getName()} WON`;
+                    }
+                }else{
+                    playerTurn.innerText = `${player1.getName()} turn`;
+                    activePlayer = 0; 
+                }    
+            }
+        }
+    }
+
+    for(let i = 0; i < squareGrid.length; i++){
+        squareGrid[i].addEventListener('click', changeTurn);
+    }
+
+})();
+
 
 
 const displayController = (function(){
@@ -29,9 +118,7 @@ const displayController = (function(){
     const okSubmit = document.querySelectorAll('button[type=submit');
     const playerLabel1 = document.querySelector('.player1');
     const playerLabel2 = document.querySelector('.player2');
-
-    const player1 = Player('Player 1', 'X');
-    const player2 = Player('Player 2', 'O');
+    
 
     function test(e){
         e.preventDefault();
@@ -51,6 +138,7 @@ const displayController = (function(){
         playerLabel2.innerText = player2.getName();
     }
     
+
     okSubmit.forEach(ok =>{
         ok.addEventListener('click', test)
     });
@@ -59,190 +147,59 @@ const displayController = (function(){
 
 
 
-/*---------------------------------------------------------------------------*/
-const gridboard = ["0", "1", "2",
-                   "3", "4", "5",
-                   "6", "7", "8",];
-
-
-                   
-
-
-const winPossibilities = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
-
-/*
-winPossibilities.forEach((item, index) =>{
-    log(`${index} : ${item}`);
-})
-
-
-const equalsIgnoreOder = function(a, b){
-    if(a.length != b.length) return false;
-    const uniqueValues = new Set([...a, ...b]);
-    for(const v of uniqueValues){
-        const aCount = a.filter(e => e === v).length;
-        const bCount = b.filter(e => e === v).length;
-        if(aCount !== bCount) return false;       
-    }
-    return true;
-}
-
-let table = [];
-for(let i = 0; i < winPossibilities.length; i++){
-    table[i] = equalsIgnoreOder(winPossibilities[i], [0, 8, 4]);
-}
-
-log(table)
-*/
-
-
-
-
-/**
- * Pour un joeur 
- * 
-*/
-
-
-
-const squareGrid = document.querySelectorAll('.square');
-const player1 = Player('Player 1', 'X');
-const player2 = Player('Player 2', 'O');
-let activePlayer = 0;
-let players = [player1.getSigne(), player2.getSigne()]
-
-const Xres = [];
-const Ores = [];
-
-
-function game(){
-    for(let i = 0; i < squareGrid.length; i++){
-        squareGrid[i].addEventListener('click', changeTurn);
-    }
-}
-
-function changeTurn(squareChosen){ 
-    let squareClickedId = squareChosen.target.dataset.square;
-    const squareClicked = document.getElementById(squareClickedId)
-    
-
-    
-    /*--- check for the win logic --- */
-    function checkWin(arg){
-        if(arg == 'X'){
-            Xres.push(squareClickedId);
-        }else{
-            Ores.push(squareClickedId);
-        }
-    }
-        
-        /*
-        winPossibilities.forEach((wCondition) => {
-            if(wCondition.every(item => (Xres.includes(item)))){
-                log(Xres);
-                log('WINNER X');
-            }
-            if(wCondition.every(item => (Ores.includes(item)))){
-                log(Ores);
-                log('WINNER Y');
-            }
-        }); */
-
-
-
-       /* winningPossibilities.forEach( (possibilitie) =>{
-            if(possibilitie.every( (arg) =>{
-                Xres.includes(arg)
-            })){
-                alert('Winner')
-            }
-
-            if(possibilitie.every( (arg) =>{
-                Ores.includes(arg)
-            })){
-                alert('Winner')
-            }
-        }) */
-
-    /*--------------------------------*/
- 
-    if(activePlayer == 0){
-        if(squareClicked.innerText.includes('X') || squareClicked.innerText.includes('O')){
-            return;
-        }else{
-            squareClicked.innerText = players[activePlayer];
-            checkWin(players[activePlayer]);
-            activePlayer = 1  
-        }
-    }else{
-        if(squareClicked.innerText.includes('X') || squareClicked.innerText.includes('O')){
-            return;
-        }else{
-            squareClicked.innerText = players[activePlayer];
-            checkWin(players[activePlayer]);
-            activePlayer = 0;   
-        }
-    }
-
-}
 
 /*-----------------------------------------------------------------------------------------------------------------------------------------------*/
 
+const Module = (function(){
+    let isArrayEqual = false;  
 
-const arrayTotal = [4, 2, 1, 5, 8];
+    const _createAllTripletOfArray = function(arrayTotal){
+        let combinationArray = [];
+        arrayTotal.sort();
 
-let combinationArray = [];
-
-arrayTotal.sort();
-
-
-for (let i = 0; i < (arrayTotal.length - 2); i++) {
-
-  for (let j = (i + 1); j < (arrayTotal.length - 1); j++) {
-
-    for (let k = (j + 1); k < arrayTotal.length; k++) {
-
-      combinationArray.push([arrayTotal[i], arrayTotal[j], arrayTotal[k]])
+        for (let i = 0; i < (arrayTotal.length - 2); i++) {
+            for (let j = (i + 1); j < (arrayTotal.length - 1); j++) {          
+              for (let k = (j + 1); k < arrayTotal.length; k++) {          
+                    combinationArray.push([arrayTotal[i], arrayTotal[j], arrayTotal[k]])
+              }
+            }
+        }
+        return combinationArray;
     }
-  }
-}
- console.log(combinationArray);
-
-const a = [1, 2, 3, 5];
-const b = [1, 2, 3, 5];
-let result = false
-
-function isEqual(a, b){
-    // If length is not equal
-    if(a.length!=b.length)
-     return result;
-    else{
-     
-    // Comparing each element of array
-     for(let i=0;i<a.length;i++)
-     if(a[i]!=b[i])
-      return result;
-      return !result;
+                       
+    const _findEqualArray = function(a, b){
+        if(a.length!=b.length)
+            return isArrayEqual;
+        else{
+            for(let i=0;i<a.length;i++)
+                if(a[i]!=b[i]) return isArrayEqual;    
+            return !isArrayEqual;
+        }
     }
-}
 
+    function compareWinPossibilityToXOresult(winPos, randomArray){
+        const combinationArray = _createAllTripletOfArray(randomArray)
 
-
-for(let i = 0; i < winPossibilities.length; i++){
-    for(let j = 0; j <combinationArray.length; j++){
-        if(isEqual(winPossibilities[i], combinationArray[j]) == true){
-            log('HOURRAH');
-            log(`(${i} : ${winPossibilities[i]}), (${j} : ${combinationArray[j]})`)
-            break;
-        };
+        for(let i = 0; i < winPos.length; i++){
+            for(let j = 0; j <combinationArray.length; j++){
+                if(_findEqualArray(winPos[i], combinationArray[j]) == true){
+                     return true;
+                }
+            }
+        }
     }
-}
+
+    return {compareWinPossibilityToXOresult}
+})();
+
+
+
+
+
+
+/* -------------------------------------Reset the Game------------------------------------------ */
+
+const resetButton = document.querySelectorAll('footer > button');
+/*
+resetButton.addEventListener('click', resetAll()); */
+
